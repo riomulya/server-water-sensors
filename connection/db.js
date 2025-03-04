0fad18eb055754b2c16cacd8e3f501f525aee5b7
Rio Mulya Syawal <riomulya75@gmail.com>
2025-03-04 19:47:56 +0700

    update hosting database

--- connection/db.js
+++ connection/db.js
@@ -1,11 +1,19 @@
 const mysql = require('mysql2');
 
 // Konfigurasi koneksi ke database MySQL
+// const db = mysql.createPool({
+//   host: 'localhost', // Ganti dengan host MySQL Anda
+//   user: 'root', // Ganti dengan user MySQL Anda
+//   password: '', // Ganti dengan password MySQL Anda
+//   database: 'db_watersensors', // Nama database Anda
+// });
+
+// Hosting
 const db = mysql.createPool({
-  host: 'localhost', // Ganti dengan host MySQL Anda
-  user: 'root', // Ganti dengan user MySQL Anda
-  password: '', // Ganti dengan password MySQL Anda
-  database: 'db_watersensors', // Nama database Anda
+  host: 'baczd.h.filess.io', // Ganti dengan host MySQL Anda
+  user: 'watersensors_tinysport', // Ganti dengan user MySQL Anda
+  password: '6eeb9bfec80a161e71774d1ec2665527da29121b', // Ganti dengan password MySQL Anda
+  database: 'watersensors_tinysport', // Nama database Anda
 });
 
 // Cek koneksi
